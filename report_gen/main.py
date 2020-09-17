0aa46d2e54dfffddc46c21d7bdac15b9d62e9762
hexagramg <hexagramg@gmail.com>
2020-09-17 19:00:42 +0300

    more advanced report generation

--- report_gen/main.py
+++ report_gen/main.py
@@ -1,32 +1,59 @@
 import weasyprint as wp
 from jinja2 import Environment, FileSystemLoader
 import pandas as pd
+import os
+path = os.path.abspath('../')
+os.chdir(path)
 from webcomponent.stocks_connector.analytics import DataAnalysisYF
 import asyncio
 from settingscomponent.loader import SEQURITIES
 import numpy as np
+from bs4 import BeautifulSoup as bs
+seq_lots = ['moex', 'inter']
 
-html = '''<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>{{ title }}</title>
-</head>
-<body>
-    <h2>probably a test of a table</h2>
-     {{ test_table}}
-</body>
-</html>'''
 
 env = Environment(loader=FileSystemLoader('./report_gen'))
 template = env.get_template('template.html')
 loop = asyncio.get_event_loop()
-data = loop.run_until_complete(DataAnalysisYF.create(SEQURITIES['moex']))
 
+
+async def get_sequrities():
+    tasks = [DataAnalysisYF.create(SEQURITIES[lot]) for lot in seq_lots]
+    data = await asyncio.gather(*tasks)
+    return data
+
+data_seq = loop.run_until_complete(get_sequrities())
+
+formatted_data = []
+for seq, data in zip(seq_lots, data_seq):
+    html = data.stats_df.to_html(float_format=lambda x: '%.2f' % x)
+    souped_table: bs = bs(html)
+
+    tds = souped_table.find_all('td')
+    for td in tds:
+        value = td.string
+        try:
+            number = float(value)
+        except ValueError as e:
+            if len(value) > 1:
+                if value[-1] == '/':
+                    td.string = value[:-1]
+        else:
+            if 'class' in td:
+                prev_class = td['class'] + ' '
+            else:
+                prev_class = ''
+            if number > 0:
+                td['class'] = prev_class + 'plus'
+            elif number < 0:
+                td['class'] = prev_class + 'minus'
+
+    formatted_data.append((seq, souped_table.prettify()))
 
 template_var = {
     'title': 'TESSSST',
-    'test_table': data.stats_df.to_html(float_format=lambda x: '%.2f' % x)
+    'roi_tables': formatted_data,
+    'seq': seq_lots
 }
 html_out = template.render(template_var)
 wp.HTML(string=html_out).write_pdf("report.pdf", stylesheets=['report_gen/typography.css'])
